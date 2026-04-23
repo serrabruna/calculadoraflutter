4eae0d4676422858fea885d893dae35aca02d35a
serrabruna <brunaibserra@hotmail.com>
2026-04-23 14:53:55 -0300

    separação de responsabilidade: criação de calculadoraPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculadoraflutter/pages/calculadora_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,38 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const Calculadora(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class Calculadora extends StatefulWidget {
-  const Calculadora({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<Calculadora> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<Calculadora> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            readOnly: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.blue[100],
-            ),
-          ),
-        ],
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.deepPurple),
+      home: const CalculadoraPage(),
     );
   }
 }
